0c538d158f29b26b90cb43afaead210483d806bb
DSTTSD <1720613556@qq.com>
2023-07-06 07:10:32 +0000

    not doing well for deepspeed

--- deploy2.sh
+++ deploy2.sh
@@ -1,4 +1,5 @@
+export CUDA_VISIBLE_DEVICES="0,1"
 controller_port=21001
 model_port=21002
-openai_port=8000
-python3 -m fastchat.serve.openai_api_server --host localhost --port $openai_port --controller-address http://localhost:${controller_port}
+openai_port=8001
+python3 -m fastchat.serve.openai_api_server --host localhost --port $openai_port --controller-address http://localhost:${controller_port} 
